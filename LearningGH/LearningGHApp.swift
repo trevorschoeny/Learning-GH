42ad9af58dd8108b6286bd4223fadd74a485e63c
Trevor Schoeny <trevorschoeny1@gmail.com>
2021-05-13 12:17:32 -0400

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Trevor Schoeny on 5/13/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
